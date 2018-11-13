b2c28cbcbc17bcd9d5fdd26acfb7f1f66c772464
linan <linan.groot@outlook.com>
2018-11-14 07:45:32 +0800

    pom file and web file

--- src/main/java/com/system/controller/HelloWorld.java
+++ src/main/java/com/system/controller/HelloWorld.java
@@ -1,5 +1,17 @@
 package com.system.controller;
 
-public class HelloWorld {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+/**
+ *
+ */
+@Controller
+@RequestMapping(value = "/")
+public class HelloWorld {
+    public void test (){
+        ModelAndView mv = new ModelAndView();
+//        mv.setViewName();
+    }
 }
